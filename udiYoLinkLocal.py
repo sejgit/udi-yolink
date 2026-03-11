bdf2739d3ab1d671569e94ca516e94e8b993f277
Panda88CO <65139210+Panda88CO@users.noreply.github.com>
2026-03-11 12:08:28 -0700

    test

--- udiYoLinkLocal.py
+++ udiYoLinkLocal.py
@@ -12,7 +12,7 @@ from yoLink_init_V4 import YoLinkInitPAC
 try:
     import udi_interface
     loggingi = udi_interface.LOGGER
-    loggingi.setLevel(level=loggingi.INFO)
+    loggingi.setLevel(30)
     logging = udi_interface.NLOGGER
     Custom = udi_interface.Custom
 except ImportError:
