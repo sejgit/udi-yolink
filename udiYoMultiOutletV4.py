b6d0c6d3f5db52da92a5979e55b3bb60b606376e
Panda88CO <65139210+Panda88CO@users.noreply.github.com>
2026-04-01 12:03:00 -0700

    Update udiYoMultiOutletV4.py

--- udiYoMultiOutletV4.py
+++ udiYoMultiOutletV4.py
@@ -571,6 +571,8 @@ class udiYoMultiOutlet(udi_interface.Node):
             #logging.debug(self.subnodeAdr)
             time.sleep(1)
             self.yoMultiOutlet.initNode()
+            sch_address = self.address[4:14] + '_SCH'
+            sch_address = self.poly.getValidAddress(sch_address)
             self.schedule = udiYoSchedule( self.poly, self.address, sch_address, 'Schedules' , self.yoAccess, self.devInfo)
             self.adr_list.append(sch_address)
             time.sleep(2)
