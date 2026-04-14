ed5b21980d8a95795267c289d649810536395498
Christian Olgaard <colgaard@litepoint.com>
2026-04-14 12:20:32 -0700

    Update udiYoWaterMeterMultiControllerV4.py

--- udiYoWaterMeterMultiControllerV4.py
+++ udiYoWaterMeterMultiControllerV4.py
@@ -402,7 +402,7 @@ class udiYoSubWaterMeter(udi_interface.Node):
     def updateData(self):
         try:
             if self.node is not None:
-                while not self.node_ready or not self.system_ready or not self.configDone:
+                while not self.sub_node_ready or not self.system_ready or not self.configDone:
                     time.sleep(0.5)
                 message_type, message_action = self.yoWaterCtrl.get_message_type()
                 if self.yoWaterCtrl.check_system_online():
