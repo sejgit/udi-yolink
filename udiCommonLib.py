2ee09c8e9a6960dbde9b56b3657eae3b4009d453
Panda88CO <65139210+Panda88CO@users.noreply.github.com>
2026-04-17 15:04:01 -0700

    Update udiCommonLib.py

--- udiCommonLib.py
+++ udiCommonLib.py
@@ -3,7 +3,7 @@
 Yolink Control Main Node  program 
 MIT License
 """
-version = '1.8.32'
+version = '1.8.33'
 import sys
 import re
 import time
@@ -157,7 +157,7 @@ def addNodes (self, deviceList) -> list:
     #supportedYoTypes = ['SprinklerV2', 'Sprinkler', 'Thermostat', 'SoilThcSensor', 'THSensor' ]     
     #supportedYoTypes = ['WaterMeterController', 'WaterMeterMultiController']   
     #supportedYoTypes = ['WaterMeterMultiController']     
-    supportedYoTypes = ['Switch', 'InfraredRemoter']
+    #supportedYoTypes = ['Switch', 'InfraredRemoter']
     remove_list= []
     schedule_queue = []
     for dev in deviceList:
